0b2a258cc9cd981b4777425570b8f9c6732f1881
Vicente Contreras <vicentecontreras19@yahoo.com>
2025-01-22 23:39:23 -0800

    initial commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
+    },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "user",
+    },
+
+    //createdAt,updatedAt
+  },
+  { timestamps: true }
+);
